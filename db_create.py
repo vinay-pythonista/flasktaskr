2cd6c1e6a4218ef4589012c9bd8b6c2270152483
XpythonistaX <invisible-man1@outlook.com>
2020-07-06 16:27:34 +0530

    Password hashing and custom error pages

--- db_create.py
+++ db_create.py
@@ -1,16 +1,16 @@
-# project/db_create.py
+# db_create.py
 
 
 from project import db
-from project.models import Task, User
-from datetime import date
+#from project.models import Task, User
+#from datetime import date
 
 
 # create the database and the db table
 db.create_all()
 
-db.session.add(User("admin", "ad@min.com", "admin", "admin"))
-db.session.add(Task("Finish this tutorial", date(2016, 9, 22), 10, date(2015, 2, 13), 1, 1))
-db.session.add(Task("Finish Real Python", date(2016, 10, 3), 10, date(2015, 2, 13), 1, 1))
+#db.session.add(User("admin", "ad@min.com", "admin", "admin"))
+#db.session.add(Task("Finish this tutorial", date(2016, 9, 22), 10, date(2015, 2, 13), 1, 1))
+#db.session.add(Task("Finish Real Python", date(2016, 10, 3), 10, date(2015, 2, 13), 1, 1))
 
 db.session.commit()
--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -4,7 +4,7 @@
 import os
 import unittest
 
-from project import app, db
+from project import app, db, bcrypt
 from project._config import basedir
 from project.models import Task, User
 
@@ -21,6 +21,7 @@ class TasksTests(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
             os.path.join(basedir, TEST_DB)
         self.app = app.test_client()
@@ -51,7 +52,11 @@ class TasksTests(unittest.TestCase):
         return self.app.get('logout/', follow_redirects=True)
 
     def create_user(self, name, email, password):
-        new_user = User(name=name, email=email, password=password)
+        new_user = User(
+        	name=name,
+        	email=email,
+        	password=bcrypt.generate_password_hash(password)
+        )
         db.session.add(new_user)
         db.session.commit()
 
@@ -68,7 +73,7 @@ class TasksTests(unittest.TestCase):
         new_user = User(
             name='Superman',
             email='admin@realpython.com',
-            password='allpowerful',
+            password=bcrypt.generate_password_hash('allpowerful'),
             role='admin'
         )
         db.session.add(new_user)
@@ -209,6 +214,51 @@ class TasksTests(unittest.TestCase):
         for task in tasks:
             self.assertEqual(task.name, 'Run around in circles')
 
+    def test_task_template_displays_logged_in_user_name(self):
+    	self.register('Fletcher', 'fletcher@realpython.com', 'python101', 'python101')
+    	self.login('Fletcher', 'python101')
+    	response = self.app.get('tasks/', follow_redirects=True)
+    	self.assertIn(b'Fletcher', response.data)
+
+    def test_users_cannot_see_task_modify_links_for_tasks_not_created_by_them(self):
+    	self.register('Michael', 'michael@realpython.com', 'python', 'python')
+    	self.login('Michael', 'python')
+    	self.app.get('tasks/', follow_redirects=True)
+    	self.create_task()
+    	self.logout()
+    	self.register('Fletcher', 'fletcher@realpython.com', 'python101', 'python101')
+    	response = self.login('Fletcher', 'python101')
+    	self.app.get('tasks/', follow_redirects=True)
+    	self.assertNotIn(b'Mark as complete', response.data)
+    	self.assertNotIn(b'Delete', response.data)
+
+    def test_users_can_see_task_modify_links_for_tasks_created_by_them(self):
+    	self.register('Michael', 'michael@realpython.com', 'python', 'python')
+    	self.login('Michael', 'python')
+    	self.app.get('tasks/', follow_redirects=True)
+    	self.create_task()
+    	self.logout()
+    	self.register('Fletcher', 'fletcher@realpython.com', 'python101', 'python101')
+    	self.login('Fletcher', 'python101')
+    	self.app.get('tasks/', follow_redirects=True)
+    	response = self.create_task()
+    	self.assertIn(b'complete/2/', response.data)
+    	self.assertIn(b'delete/2/', response.data)
+
+    def test_admin_users_can_see_task_modify_links_for_all_tasks(self):
+    	self.register('Michael', 'michael@realpython.com', 'python', 'python')
+    	self.login('Michael', 'python')
+    	self.app.get('tasks/', follow_redirects=True)
+    	self.create_task()
+    	self.logout()
+    	self.create_admin_user()
+    	self.login('Superman', 'allpowerful')
+    	self.app.get('tasks/', follow_redirects=True)
+    	response = self.create_task()
+    	self.assertIn(b'complete/1/', response.data)
+    	self.assertIn(b'delete/1/', response.data)
+    	self.assertIn(b'complete/2/', response.data)
+    	self.assertIn(b'delete/2/', response.data)
 
 if __name__ == "__main__":
     unittest.main()
